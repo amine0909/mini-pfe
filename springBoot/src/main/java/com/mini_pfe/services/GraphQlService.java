b9e7ef63c2eba3b1facb94b2e3896c04f8ae446f
unknown <aminbj34@gmail.com>
2018-11-14 18:46:51 +0100

    graphql added

--- springBoot/src/main/java/com/mini_pfe/services/GraphQlService.java
+++ springBoot/src/main/java/com/mini_pfe/services/GraphQlService.java
@@ -27,17 +27,7 @@ public class GraphQlService {
     @Value("classpath:graphql/Materiel.graphqls")
     Resource resourceMateriel;
 
-    @Value("classpath:graphql/Reclamation.graphqls")
-    Resource resourceReclamation;
 
-    @Value("classpath:graphql/Departement.graphqls")
-    Resource resourceDepartement;
-
-    @Value("classpath:graphql/Classe.graphqls")
-    Resource resourceClasse;
-
-    @Value("classpath:graphql/ChefDepartement.graphqls")
-    Resource resourceChefDep;
 
     private GraphQL graphQL;
 
@@ -47,25 +37,11 @@ public class GraphQlService {
     // load schema at application start up
     @PostConstruct
     private void loadSchema() throws IOException {
-        SchemaParser schemaParser = new SchemaParser();
-        // get the schema
-        File schemaFileMateriel = this.resourceMateriel.getFile();
-        File schemaFileReclamation = this.resourceReclamation.getFile();
-        File schemaFileClasse = this.resourceClasse.getFile();
-        File schemaFileDepart = this.resourceDepartement.getFile();
-        File schemaFileChefDep = this.resourceChefDep.getFile();
         // parse schema
-        //TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schemaFile);
-        TypeDefinitionRegistry typeDefinitionRegistry = new TypeDefinitionRegistry();
-        typeDefinitionRegistry.merge(schemaParser.parse(schemaFileMateriel));
-        typeDefinitionRegistry.merge(schemaParser.parse(schemaFileReclamation));
-        typeDefinitionRegistry.merge(schemaParser.parse(schemaFileClasse));
-        typeDefinitionRegistry.merge(schemaParser.parse(schemaFileDepart));
-        typeDefinitionRegistry.merge(schemaParser.parse(schemaFileChefDep));
-
+        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(resourceMateriel.getFile());
 
         RuntimeWiring wiring = buildRuntimeWiring();
-        GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeDefinitionRegistry, wiring);
+        GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeRegistry, wiring);
         graphQL = GraphQL.newGraphQL(schema).build();
     }
 
